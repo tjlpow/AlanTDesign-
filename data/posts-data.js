4cc2238917e1bf43ea42a6bc0cc7a1b67f33f498
tjlpow <tjlevil@gmail.com>
2017-07-19 14:25:06 +0800

    finish content

--- data/posts-data.js
+++ data/posts-data.js
@@ -1,5 +1,7 @@
 var article1 = require('/article1.js')
 var article2 = require('/article2.js')
+var article3 = require('/article3.js')
+var article4 = require('/article4.js')
 
 var localData = [
   {
@@ -39,14 +41,32 @@ var localData = [
     author: "原创文章",
     headImageSrc: "/images/post/xiaolong.jpg",
     avatar: "/images/avatar/4.png",
-    detail: "",
+    detail: article3.article,
     postId: 2,
     music: {
       title: "音乐2",
       dataUrl: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
       coverImgUrl: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
     }
+  },
+
+  {
+    title: "iOS8 Human Interface Guidelines 精选阅读",
+    imgSrc: "/images/post/xiaolong.jpg",
+    date: "4月3日",
+    author: "原创文章",
+    headImageSrc: "/images/post/xiaolong.jpg",
+    avatar: "/images/avatar/4.png",
+    detail: article4.article,
+    postId: 3,
+    music: {
+      title: "音乐2",
+      dataUrl: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
+      coverImgUrl: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+    }
   }
+
+ 
 ]
 
 
